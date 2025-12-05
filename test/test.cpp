33aeff105245256ff555260bf16c04ae0a94450f
AshtynConnoley <ashtyn.connoley@gmail.com>
2025-12-05 18:25:12 -0500

    Untested Test cases

--- test/test.cpp
+++ test/test.cpp
@@ -3,82 +3,82 @@
 
 // change if you choose to use a different header name
 #include "CampusCompass.h"
-
 using namespace std;
 
-// the syntax for defining a test is below. It is important for the name to be
-// unique, but you can group multiple tests with [tags]. A test can have
-// [multiple][tags] using that syntax.
-TEST_CASE("Example Test Name - Change me!", "[tag]") {
-  // instantiate any class members that you need to test here
-  int one = 1;
+TEST_CASE("Inavlid commands") {
+    CampusCompass compass;
+    compass.parseCSV("../data/edges.csv", "../data/classes.csv");
 
-  // anything that evaluates to false in a REQUIRE block will result in a
-  // failing test
-  REQUIRE(one == 0); // fix me!
+    // invalid command
+    REQUIRE(compass.parseCommand("notReal \"Ashtyn\" 12341234 1 1 COP3530") == false);
 
-  // all REQUIRE blocks must evaluate to true for the whole test to pass
-  REQUIRE(false); // also fix me!
+    // invalid ID
+    REQUIRE(compass.parseCommand("insert \"Ashtyn\" 1234L234 1 1 COP3530") == false);
+
+    // Invalid Name
+    REQUIRE(compass.parseCommand("insert \"Ashtyn3\" 12345678 1 1 COP3530") == false);
+
+    // Wrong number of classes
+    REQUIRE(compass.parseCommand("insert \"Ashtyn\" 12341234 1 2 COP3530") == false);
+
+    // Invalid class codes
+    REQUIRE(compass.parseCommand("insert \"Ashtyn\" 12345678 1 1 cop3530") == false);
+    REQUIRE(compass.parseCommand("insert \"Ashtyn\" 12345678 1 1 COPP3530") == false);
 }
 
-TEST_CASE("Test 2", "[tag]") {
-  // you can also use "sections" to share setup code between tests, for example:
-  int one = 1;
+TEST_CASE("Edge Cases") {
+    CampusCompass compass;
+    compass.parseCSV("../data/edges.csv", "../data/classes.csv");
+    bool insertion = compass.insert("Ashtyn", 12345678, "1", {"COP3530"});
+    REQUIRE(insertion == true);
 
-  SECTION("num is 2") {
-    int num = one + 1;
-    REQUIRE(num == 2);
-  };
+    // Student doesn't exist
+    REQUIRE(compass.remove(12345679) == false);
 
-  SECTION("num is 3") {
-    int num = one + 2;
-    REQUIRE(num == 3);
-  };
+    // replace nonexistent class
+    REQUIRE(compass.replaceClass(12345678, "GAP3232", "COP3504") == false);
 
-  // each section runs the setup code independently to ensure that they don't
-  // affect each other
+    // Student is deleted after dropping last class
+    REQUIRE(compass.dropClass(12345678, "COP3530") == true);
+    REQUIRE(compass.remove(12345678) == false);
 }
 
-// You must write 5 unique, meaningful tests for credit on the testing section
-// of this project!
+TEST_CASE("dropClass, removeClass, remove, and replaceClass") {
+    CampusCompass compass;
+    compass.parseCSV("../data/edges.csv", "../data/classes.csv");
+    REQUIRE(compass.insert("Brandon", 11110000, "1", {"COP3530", "COP3502", "COT3100"}) == true);
+    REQUIRE(compass.insert("Sarah",   22220000, "2", {"COP3530", "COP3503"}) == true);;
 
-// See the following for an example of how to easily test your output.
-// Note that while this works, I recommend also creating plenty of unit tests for particular functions within your code.
-// This pattern should only be used for final, end-to-end testing.
+    // removeClass test
+    int removed = compass.removeClass("COP3530");
+    REQUIRE(removed == 2);
 
-// This uses C++ "raw strings" and assumes your CampusCompass outputs a string with
-//   the same thing you print.
-TEST_CASE("Example CampusCompass Output Test", "[flag]") {
-  // the following is a "raw string" - you can write the exact input (without
-  //   any indentation!) and it should work as expected
-  // this is based on the input and output of the first public test case
-  string input = R"(6
-insert "Student A" 10000001 1 1 COP3502
-insert "Student B" 10000002 1 1 COP3502
-insert "Student C" 10000003 1 2 COP3502 MAC2311
-dropClass 10000001 COP3502
-remove 10000001
-removeClass COP3502
-)";
+    // dropClass test
+    REQUIRE(compass.dropClass(22220000, "COP3503") == true);
+    REQUIRE(compass.remove(22220000) == false);
 
-  string expectedOutput = R"(successful
-successful
-successful
-successful
-unsuccessful
-2
-)";
+    // replaceClass and remove tests
+    REQUIRE(compass.replaceClass(11110000, "COP3502", "COP3503") == true);
+    REQUIRE(compass.dropClass(11110000, "COP3503") == true);
+    REQUIRE(compass.remove(11110000) == true);
 
-  string actualOutput;
+}
 
-  // somehow pass your input into your CampusCompass and parse it to call the
-  // correct functions, for example:
-  /*
-  CampusCompass c;
-  c.parseInput(input)
-  // this would be some function that sends the output from your class into a string for use in testing
-  actualOutput = c.getStringRepresentation()
-  */
+TEST_CASE("printShortestEdges test") {
+    CampusCompass compass;
+    compass.parseCSV("../data/edges.csv", "../data/classes.csv");
 
-  REQUIRE(actualOutput == expectedOutput);
+    compass.insert("Ashtyn", 11111111, "43", {"PHY2048", "IDS2935"}); // PHY2048 only has 1 edge
+
+    // Testing with valid path to both
+    vector<pair<string, int>> result = compass.printShortestEdges(11111111);
+    vector<pair<string, int>> expected = {make_pair("IDS2935", 2), make_pair("PHY2048", 9)};
+    REQUIRE(result == expected);
+
+    // Testing with valid path to only one class
+    compass.toggleEdgeClosure("49", "56");
+    REQUIRE(compass.checkEdgeStatus("49", "56") == 0);
+    result = compass.printShortestEdges(11111111);
+    expected = {make_pair("IDS2935", 2), make_pair("PHY2048", -1)};
+    REQUIRE(result == expected);
 }
